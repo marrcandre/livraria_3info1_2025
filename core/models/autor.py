a35b3f4a058075713319557c99a4039eb64a1877
Marco André Mendes <marcoandre@gmail.com>
2025-04-04 09:28:50 -0300

    feat: criação da API do Autor.

---
+++ core/models/autor.py
@@ -0,0 +1,15 @@
+from tabnanny import verbose
+from django.db import models
+
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f"({self.id}) {self.nome}"
+
+    class Meta:
+        verbose_name = "Autor"
+        verbose_name_plural = "Autores"
+
